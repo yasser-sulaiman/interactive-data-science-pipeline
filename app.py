66a6e65c7b1bf9ce29aa5636d4e091a6d17f3748
Yasser Sulaiman <YasserSulaiman.is@gmail.com>
2023-08-22 23:31:24 +0200

    moving header to the main app layout

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 from dash import Dash
+from dash import html, dcc
+import dash
 
 app = Dash(__name__, use_pages=True)
 
 def main():
     app = Dash(__name__, use_pages=True)
     app.title = "Interactive Data Science Pipeline"
+
+    app.layout = html.Div([
+        # Header
+        html.Div(
+            [
+                html.H1(
+                    children='Welcome to HahoML',
+                    style={'textAlign': 'center'},
+                ),
+                html.P(
+                    children='Where ML made easier than ever!',
+                    style={'textAlign': 'center'},
+                ),
+                html.Div(
+                    dcc.Link("Train", href="/")
+                ),
+                html.Div(
+                    dcc.Link(f"Visualize", href="/visualization")
+                )
+            ],
+            className='header-container',
+        ),
+
+        html.Hr(),
+
+        dash.page_container
+    ])
     app.run(debug=True)
 
 if __name__ == '__main__':
